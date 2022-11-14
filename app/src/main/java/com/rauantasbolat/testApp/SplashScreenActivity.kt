c1756acb8357bbc3c36ade317312af80b466652b
rauantasbolat <rauan98ktl@gmail.com>
2022-11-15 01:27:37 +0600

    bg

--- app/src/main/java/com/rauantasbolat/testApp/SplashScreenActivity.kt
+++ app/src/main/java/com/rauantasbolat/testApp/SplashScreenActivity.kt
@@ -1,16 +1,12 @@
 package com.rauantasbolat.testApp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.rauantasbolat.testApp.ui.MainActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.test_layout)
 
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
     }
 }
